a82ec77420381e8be1e637d8fa117ed2a9599e80
CatGames2281-cell <pastushokroma7@gmail.com>
2026-02-06 08:51:03 +0200

    Update authentication.php

--- classes/watrlabs/authentication.php
+++ classes/watrlabs/authentication.php
@@ -205,7 +205,14 @@ class authentication {
                 'expiration' => $expiration
             );
             $insertId = $db->table('sessions')->insert($data);
-            setcookie(".ROBLOSECURITY", $session, $expiration, "/", "." . $_ENV["APP_DOMAIN"]);
+            setcookie(".ROBLOSECURITY", $session, [
+               'expires' => $expiration,
+               'path' => '/',
+               'domain' => '.' . $_ENV["APP_DOMAIN"],
+               'secure' => true,
+               'httponly' => true,
+               'samesite' => 'Lax'
+            ]);
         } else {
 
             $data = array(
